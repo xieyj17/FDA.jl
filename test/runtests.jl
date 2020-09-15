a9cce09a4dd0f44419130a3b8356799e377303d8
Yijun Xie <yijun.xie@uwaterloo.ca>
2020-09-15 03:17:40 -0400

    add example

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using FDA
 using Test
+using CSV
 
 @testset "FDA.jl" begin
-    # Write your tests here.
+    df = CSV.read("sample.csv")
+    y = df[:,2:251];
+
+    temp_basis = gen_fourier_basis([0,1],15)
+    argvals = range(0,1;length = 101)
+    temp_coefs = Data2fd(argvals, y, temp_basis)
+
+    smooth_curve = eval_fd(argvals, temp_coefs)
+
+    plot(argvals, smooth_curve[:,1], label = "Smoothed curve", lw = 3)
+    plot!(argvals, y[:,1],  label = "Observed curve", lw = 3)
 end
